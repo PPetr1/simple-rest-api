667444980d510ac8f3528b20d32a68a5dd0916f7
Petr Půta <petr.puta1@gmail.com>
2024-04-16 15:15:35 +0200

    Created DTO for error message

---
+++ simple-REST-Api/src/main/java/dtos/ErrorResponseDTO.java
@@ -0,0 +1,20 @@
+package dtos;
+
+public class ErrorResponseDTO {
+    private String errorMessage;
+
+    public ErrorResponseDTO() {
+    }
+
+    public ErrorResponseDTO(String error) {
+        this.errorMessage = error;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
