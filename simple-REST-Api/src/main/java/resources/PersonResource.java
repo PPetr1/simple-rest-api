d75a2df146540fb166bcdddfe84f46f16b4f6fb7
Petr Půta <petr.puta1@gmail.com>
2024-04-16 13:13:43 +0200

    createPerson endpoint

--- simple-REST-Api/src/main/java/resources/PersonResource.java
+++ simple-REST-Api/src/main/java/resources/PersonResource.java
@@ -1,15 +1,25 @@
 package resources;
 
+import dtos.CreatePersonRequestDTO;
+import dtos.ResponseMessageDTO;
 import jakarta.ejb.EJB;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import services.PersonService;
 
 public class PersonResource {
 
-    @EJB
-    private PersonService personService;
-    
+  @EJB private PersonService personService;
+
+  @POST
+  @Path("/createPerson")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response createPerson(CreatePersonRequestDTO requestDTO) {
+    personService.createPerson(requestDTO);
+    return Response.status(201)
+        .entity(new ResponseMessageDTO("Person was successfully saved"))
+        .build();
+  }
 }
