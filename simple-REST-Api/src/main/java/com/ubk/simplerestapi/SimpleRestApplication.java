984c8c87d4ad9d326f6f88a6df169a00ff73d9d1
Petr Půta <petr.puta1@gmail.com>
2024-04-16 12:43:27 +0200

    initial commit

---
+++ simple-REST-Api/src/main/java/com/ubk/simplerestapi/SimpleRestApplication.java
@@ -0,0 +1,9 @@
+package com.ubk.simplerestapi;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class SimpleRestApplication extends Application {
+
+}
