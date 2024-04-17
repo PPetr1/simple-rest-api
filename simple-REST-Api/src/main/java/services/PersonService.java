4bae58fca0d1575dcc64e3be3e5686ae8943e2d9
Petr Půta <petr.puta1@gmail.com>
2024-04-17 12:13:47 +0200

    Added logic for GetPersonForName

--- simple-REST-Api/src/main/java/services/PersonService.java
+++ simple-REST-Api/src/main/java/services/PersonService.java
@@ -2,6 +2,8 @@ package services;
 
 import dtos.CreatePersonRequestDTO;
 import dtos.DeletePersonRequestDTO;
+import dtos.GetPersonForNameRequestDTO;
+import dtos.PersonResponseDTO;
 import enums.Gender;
 import exceptions.ApiBadRequestException;
 import jakarta.ejb.Stateless;
@@ -12,6 +14,7 @@ import models.Person;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
@@ -27,7 +30,7 @@ public class PersonService {
     entityManager.persist(
         new Person(
             requestDTO.getName(),
-            birthdayParse(requestDTO.getBirthday()),
+            birthdayStringParseToDate(requestDTO.getBirthday()),
             Gender.valueOf(requestDTO.getGender().toUpperCase())));
   }
 
@@ -42,7 +45,7 @@ public class PersonService {
     validateGenderFormat(requestDTO.getGender());
   }
 
-  Date birthdayParse(String birthday) {
+  Date birthdayStringParseToDate(String birthday) {
     String[] arr = birthday.split("\\.");
     return java.sql.Date.valueOf(
         LocalDate.of(Integer.parseInt(arr[2]), Integer.parseInt(arr[1]), Integer.parseInt(arr[0])));
@@ -107,4 +110,33 @@ public class PersonService {
           "Provided personId: " + requestDTO.getPersonId() + " doesn't exist");
     }
   }
+
+  @Transactional
+  public Object getPersonForName(GetPersonForNameRequestDTO requestDTO) {
+    List<Person> personList =
+        entityManager
+            .createQuery("SELECT p FROM Person p WHERE p.name LIKE :name", Person.class)
+            .setParameter("name", requestDTO.getName())
+            .getResultList();
+    validateResponseSinglePerson(personList);
+    return personToPersonResponseDTO(personList.get(0));
+  }
+
+  void validateResponseSinglePerson(List<Person> personList) {
+    if (personList.isEmpty()) {
+      throw new ApiBadRequestException("There is no person by the provided name");
+    } else if (personList.size() > 1) {
+      throw new ApiBadRequestException("There are multiple people with the same name, please use different endpoint");
+    }
+  }
+
+  PersonResponseDTO personToPersonResponseDTO(Person person) {
+    return new PersonResponseDTO(person.getName(), person.getGender().toString(), birthdayFixResponseFormat(person.getBirthday().toString()));
+  }
+
+  String birthdayFixResponseFormat(String birthday) {
+    String[] arr = birthday.split("-");
+    return arr[2] + "." + arr[1] + "." + arr[0];
+  }
+
 }
