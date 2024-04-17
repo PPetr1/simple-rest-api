17aad8fad4cb153bd0995f51c3b64805eb6195ed
Petr Půta <petr.puta1@gmail.com>
2024-04-17 19:07:30 +0200

    Extracted interface from PersonService

--- simple-REST-Api/src/main/java/services/PersonService.java
+++ simple-REST-Api/src/main/java/services/PersonService.java
@@ -1,241 +1,80 @@
 package services;
 
 import dtos.*;
-import enums.Gender;
-import exceptions.ApiBadRequestException;
-import jakarta.ejb.Stateless;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import jakarta.ejb.Local;
 import jakarta.transaction.Transactional;
 import models.Person;
 
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
-@Stateless
-public class PersonService {
-
-  @PersistenceContext(name = "PersonManager")
-  private EntityManager entityManager;
-
-  public void createPerson(CreatePersonRequestDTO requestDTO) {
-    validateCreatePersonRequestDTO(requestDTO);
-    savePersonQuery(
-        new Person(
-            requestDTO.getName(),
-            birthdayStringParseToDate(requestDTO.getBirthday()),
-            Gender.valueOf(requestDTO.getGender().toUpperCase())));
-  }
+@Local
+public interface PersonService {
+  void createPerson(CreatePersonRequestDTO requestDTO);
 
   @Transactional
-  public void savePersonQuery(Person person) {
-    entityManager.persist(person);
-  }
+  void savePersonQuery(Person person);
 
-  void validateCreatePersonRequestDTO(CreatePersonRequestDTO requestDTO) {
-    if (requestDTO.getName() == null
-        || requestDTO.getGender() == null
-        || requestDTO.getBirthday() == null) {
-      throwMissingField();
-    }
-    validateNameFormat(requestDTO.getName());
-    validateBirthdayFormat(requestDTO.getBirthday());
-    validateGenderFormat(requestDTO.getGender());
-  }
+  void validateCreatePersonRequestDTO(CreatePersonRequestDTO requestDTO);
 
-  Date birthdayStringParseToDate(String birthday) {
-    String[] arr = birthday.split("\\.");
-    return java.sql.Date.valueOf(
-        LocalDate.of(Integer.parseInt(arr[2]), Integer.parseInt(arr[1]), Integer.parseInt(arr[0])));
-  }
+  Date birthdayStringParseToDate(String birthday);
 
-  void validateGenderFormat(String gender) {
-    if (gender.isEmpty()) {
-      throw new ApiBadRequestException("Gender is empty");
-    } else if (!isValidGender(gender)) {
-      throw new ApiBadRequestException("Gender can only be MALE,FEMALE or OTHER");
-    }
-  }
+  void validateGenderFormat(String gender);
 
-  boolean isValidGender(String gender) {
-    for (Gender genderEnum : Gender.values()) {
-      if (genderEnum.name().equalsIgnoreCase(gender)) {
-        return true;
-      }
-    }
-    return false;
-  }
+  boolean isValidGender(String gender);
 
-  void validateBirthdayFormat(String birthday) {
-    if (birthday.isEmpty()) {
-      throw new ApiBadRequestException("Birthday cannot be empty");
-    } else if (!isValidBirthdayPattern(birthday)) {
-      throw new ApiBadRequestException("Birthday needs to be in format:DD.MM.YYYY");
-    } else if (Integer.parseInt(birthday.substring(birthday.length() - 4))
-        > LocalDate.now().getYear()) {
-      throw new ApiBadRequestException("Person is not born yet");
-    }
-  }
+  void validateBirthdayFormat(String birthday);
 
-  Boolean isValidBirthdayPattern(String birthday) {
-    return Pattern.compile("\\b(0?[1-9]|[12][0-9]|3[01])\\.(0?[1-9]|1[012])\\.(\\d{4})\\b")
-        .matcher(birthday)
-        .matches();
-  }
+  Boolean isValidBirthdayPattern(String birthday);
 
-  void validateNameFormat(String name) {
-    if (name.isEmpty()) {
-      throw new ApiBadRequestException("Name cannot be empty");
-    } else if (!isValidNamePattern(name)) {
-      throw new ApiBadRequestException("Name cannot contain numbers or symbols");
-    } else if (name.length() > 50) {
-      throw new ApiBadRequestException("Name can have 50 characters at maximum");
-    }
-  }
+  void validateNameFormat(String name);
 
-  boolean isValidNamePattern(String name) {
-    return Pattern.compile("^[a-zA-Z ěĚšŠčČřŘžŽýÝáÁíÍéÉůŮúÚöÖ]+$").matcher(name).matches();
-  }
+  boolean isValidNamePattern(String name);
 
-  public void deletePerson(DeletePersonRequestDTO requestDTO) {
-    validateDeletePersonRequestDTO(requestDTO);
-    Optional<Person> optionalPerson = findPersonById(requestDTO.getPersonId());
-    if (optionalPerson.isPresent()) {
-      deletePersonQuery(optionalPerson.get());
-    } else {
-      throwProvideValidPersonId();
-    }
-  }
+  void deletePerson(DeletePersonRequestDTO requestDTO);
 
   @Transactional
-  void deletePersonQuery(Person person) {
-    entityManager.remove(person);
-  }
+  void deletePersonQuery(Person person);
 
-  void validateDeletePersonRequestDTO(DeletePersonRequestDTO requestDTO) {
-    if (requestDTO.getPersonId() == 0) {
-      throw new ApiBadRequestException("Please provide valid personId");
-    }
-  }
+  void validateDeletePersonRequestDTO(DeletePersonRequestDTO requestDTO);
 
   @Transactional
-  Optional<Person> findPersonById(long personId) {
-    return Optional.ofNullable(entityManager.find(Person.class, personId));
-  }
+  Optional<Person> findPersonById(long personId);
 
-  public Object getPersonForName(GetPersonForNameRequestDTO requestDTO) {
-    if (requestDTO.getName() == null) {
-      throwNameFieldIsMissing();
-    }
-    validateNameFormat(requestDTO.getName());
-    List<Person> personList = getPeopleByNameQuery(requestDTO.getName());
-    validateResponseSinglePerson(personList);
-    return personToPersonResponseDTO(personList.get(0));
-  }
+  Object getPersonForName(GetPersonForNameRequestDTO requestDTO);
 
   @Transactional
-  List<Person> getPeopleByNameQuery(String name) {
-    return entityManager
-        .createQuery("SELECT p FROM Person p WHERE p.name LIKE :name", Person.class)
-        .setParameter("name", name)
-        .getResultList();
-  }
+  List<Person> getPeopleByNameQuery(String name);
 
-  void validateResponseSinglePerson(List<Person> personList) {
-    if (personList.isEmpty()) {
-      throwNoPersonByTheName();
-    } else if (personList.size() > 1) {
-      throw new ApiBadRequestException(
-          "There are multiple people with the same name, please use /api/getPeopleByName");
-    }
-  }
+  void validateResponseSinglePerson(List<Person> personList);
 
-  PersonResponseDTO personToPersonResponseDTO(Person person) {
-    return new PersonResponseDTO(
-        person.getName(),
-        person.getGender().toString(),
-        birthdayFixResponseFormat(person.getBirthday().toString()));
-  }
+  PersonResponseDTO personToPersonResponseDTO(Person person);
 
-  String birthdayFixResponseFormat(String birthday) {
-    String[] arr = birthday.split("-");
-    return arr[2] + "." + arr[1] + "." + arr[0];
-  }
+  String birthdayFixResponseFormat(String birthday);
 
-  public PersonResponseDTOList getPeopleByName(GetPersonForNameRequestDTO requestDTO) {
-    if (requestDTO.getName() == null) {
-      throwNameFieldIsMissing();
-    }
-    validateNameFormat(requestDTO.getName());
-    List<Person> personList = getPeopleByNameQuery(requestDTO.getName());
-    validateGetPeopleByNameResponse(personList);
-    return personListToPersonResponseDTOList(personList);
-  }
+  PersonResponseDTOList getPeopleByName(GetPersonForNameRequestDTO requestDTO);
 
-  private PersonResponseDTOList personListToPersonResponseDTOList(List<Person> personList) {
-    return new PersonResponseDTOList(
-        personList.stream().map(this::personToPersonResponseDTO).collect(Collectors.toList()));
-  }
+  PersonResponseDTOList personListToPersonResponseDTOList(List<Person> personList);
 
-  void validateGetPeopleByNameResponse(List<Person> personList) {
-    if (personList.isEmpty()) {
-      throwNoPersonByTheName();
-    }
-  }
+  void validateGetPeopleByNameResponse(List<Person> personList);
 
-  void throwNoPersonByTheName() {
-    throw new ApiBadRequestException("There is no person by the provided name");
-  }
+  void throwNoPersonByTheName();
 
-  void throwNameFieldIsMissing() {
-    throw new ApiBadRequestException("name field is missing");
-  }
+  void throwNameFieldIsMissing();
 
-  void throwMissingField() {
-    throw new ApiBadRequestException("Missing field");
-  }
+  void throwMissingField();
 
-  void throwProvideValidPersonId() {
-    throw new ApiBadRequestException("Please provide valid personId");
-  }
+  void throwProvideValidPersonId();
 
-  public PersonResponseDTOList getAllPersons() {
-    List<Person> personList = getAllPersonsQuery();
-    validateGetAllPersonsResponse(personList);
-    return personListToPersonResponseDTOList(personList);
-  }
+  PersonResponseDTOList getAllPersons();
 
-  void validateGetAllPersonsResponse(List<Person> personList) {
-    if (personList.isEmpty()) {
-      throw new ApiBadRequestException("There are no people entities");
-    }
-  }
+  void validateGetAllPersonsResponse(List<Person> personList);
 
   @Transactional
-  List<Person> getAllPersonsQuery() {
-    return entityManager.createQuery("SELECT p FROM Person p", Person.class).getResultList();
-  }
+  List<Person> getAllPersonsQuery();
 
-  public void updatePerson(UpdatePersonRequestDTO requestDTO) {
-    validateUpdatePersonRequest(requestDTO);
-    Optional<Person> optionalPerson = findPersonById(requestDTO.getPersonId());
-    if (optionalPerson.isPresent()) {
-      Person person = optionalPerson.get();
-      person.setName(requestDTO.getName());
-      savePersonQuery(person);
-    } else {
-      throwProvideValidPersonId();
-    }
-  }
+  void updatePerson(UpdatePersonRequestDTO requestDTO);
 
-  void validateUpdatePersonRequest(UpdatePersonRequestDTO requestDTO) {
-    if (requestDTO.getName() == null || requestDTO.getPersonId() == 0) {
-      throwMissingField();
-    }
-    validateNameFormat(requestDTO.getName());
-  }
+  void validateUpdatePersonRequest(UpdatePersonRequestDTO requestDTO);
 }
---
+++ simple-REST-Api/src/main/java/services/PersonServiceImpl.java
@@ -0,0 +1,272 @@
+package services;
+
+import dtos.*;
+import enums.Gender;
+import exceptions.ApiBadRequestException;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import models.Person;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+@Stateless
+public class PersonServiceImpl implements PersonService {
+
+  @PersistenceContext(name = "PersonManager")
+  private EntityManager entityManager;
+
+  @Override
+  public void createPerson(CreatePersonRequestDTO requestDTO) {
+    validateCreatePersonRequestDTO(requestDTO);
+    savePersonQuery(
+        new Person(
+            requestDTO.getName(),
+            birthdayStringParseToDate(requestDTO.getBirthday()),
+            Gender.valueOf(requestDTO.getGender().toUpperCase())));
+  }
+
+  @Override
+  @Transactional
+  public void savePersonQuery(Person person) {
+    entityManager.persist(person);
+  }
+
+  @Override
+  public void validateCreatePersonRequestDTO(CreatePersonRequestDTO requestDTO) {
+    if (requestDTO.getName() == null
+        || requestDTO.getGender() == null
+        || requestDTO.getBirthday() == null) {
+      throwMissingField();
+    }
+    validateNameFormat(requestDTO.getName());
+    validateBirthdayFormat(requestDTO.getBirthday());
+    validateGenderFormat(requestDTO.getGender());
+  }
+
+  @Override
+  public Date birthdayStringParseToDate(String birthday) {
+    String[] arr = birthday.split("\\.");
+    return java.sql.Date.valueOf(
+        LocalDate.of(Integer.parseInt(arr[2]), Integer.parseInt(arr[1]), Integer.parseInt(arr[0])));
+  }
+
+  @Override
+  public void validateGenderFormat(String gender) {
+    if (gender.isEmpty()) {
+      throw new ApiBadRequestException("Gender is empty");
+    } else if (!isValidGender(gender)) {
+      throw new ApiBadRequestException("Gender can only be MALE,FEMALE or OTHER");
+    }
+  }
+
+  @Override
+  public boolean isValidGender(String gender) {
+    for (Gender genderEnum : Gender.values()) {
+      if (genderEnum.name().equalsIgnoreCase(gender)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public void validateBirthdayFormat(String birthday) {
+    if (birthday.isEmpty()) {
+      throw new ApiBadRequestException("Birthday cannot be empty");
+    } else if (!isValidBirthdayPattern(birthday)) {
+      throw new ApiBadRequestException("Birthday needs to be in format:DD.MM.YYYY");
+    } else if (Integer.parseInt(birthday.substring(birthday.length() - 4))
+        > LocalDate.now().getYear()) {
+      throw new ApiBadRequestException("Person is not born yet");
+    }
+  }
+
+  @Override
+  public Boolean isValidBirthdayPattern(String birthday) {
+    return Pattern.compile("\\b(0?[1-9]|[12][0-9]|3[01])\\.(0?[1-9]|1[012])\\.(\\d{4})\\b")
+        .matcher(birthday)
+        .matches();
+  }
+
+  @Override
+  public void validateNameFormat(String name) {
+    if (name.isEmpty()) {
+      throw new ApiBadRequestException("Name cannot be empty");
+    } else if (!isValidNamePattern(name)) {
+      throw new ApiBadRequestException("Name cannot contain numbers or symbols");
+    } else if (name.length() > 50) {
+      throw new ApiBadRequestException("Name can have 50 characters at maximum");
+    }
+  }
+
+  @Override
+  public boolean isValidNamePattern(String name) {
+    return Pattern.compile("^[a-zA-Z ěĚšŠčČřŘžŽýÝáÁíÍéÉůŮúÚöÖ]+$").matcher(name).matches();
+  }
+
+  @Override
+  public void deletePerson(DeletePersonRequestDTO requestDTO) {
+    validateDeletePersonRequestDTO(requestDTO);
+    Optional<Person> optionalPerson = findPersonById(requestDTO.getPersonId());
+    if (optionalPerson.isPresent()) {
+      deletePersonQuery(optionalPerson.get());
+    } else {
+      throwProvideValidPersonId();
+    }
+  }
+
+  @Override
+  @Transactional
+  public void deletePersonQuery(Person person) {
+    entityManager.remove(person);
+  }
+
+  @Override
+  public void validateDeletePersonRequestDTO(DeletePersonRequestDTO requestDTO) {
+    if (requestDTO.getPersonId() == 0) {
+      throw new ApiBadRequestException("Please provide valid personId");
+    }
+  }
+
+  @Override
+  @Transactional
+  public Optional<Person> findPersonById(long personId) {
+    return Optional.ofNullable(entityManager.find(Person.class, personId));
+  }
+
+  @Override
+  public Object getPersonForName(GetPersonForNameRequestDTO requestDTO) {
+    if (requestDTO.getName() == null) {
+      throwNameFieldIsMissing();
+    }
+    validateNameFormat(requestDTO.getName());
+    List<Person> personList = getPeopleByNameQuery(requestDTO.getName());
+    validateResponseSinglePerson(personList);
+    return personToPersonResponseDTO(personList.get(0));
+  }
+
+  @Override
+  @Transactional
+  public List<Person> getPeopleByNameQuery(String name) {
+    return entityManager
+        .createQuery("SELECT p FROM Person p WHERE p.name LIKE :name", Person.class)
+        .setParameter("name", name)
+        .getResultList();
+  }
+
+  @Override
+  public void validateResponseSinglePerson(List<Person> personList) {
+    if (personList.isEmpty()) {
+      throwNoPersonByTheName();
+    } else if (personList.size() > 1) {
+      throw new ApiBadRequestException(
+          "There are multiple people with the same name, please use /api/getPeopleByName");
+    }
+  }
+
+  @Override
+  public PersonResponseDTO personToPersonResponseDTO(Person person) {
+    return new PersonResponseDTO(
+        person.getName(),
+        person.getGender().toString(),
+        birthdayFixResponseFormat(person.getBirthday().toString()));
+  }
+
+  @Override
+  public String birthdayFixResponseFormat(String birthday) {
+    String[] arr = birthday.split("-");
+    return arr[2] + "." + arr[1] + "." + arr[0];
+  }
+
+  @Override
+  public PersonResponseDTOList getPeopleByName(GetPersonForNameRequestDTO requestDTO) {
+    if (requestDTO.getName() == null) {
+      throwNameFieldIsMissing();
+    }
+    validateNameFormat(requestDTO.getName());
+    List<Person> personList = getPeopleByNameQuery(requestDTO.getName());
+    validateGetPeopleByNameResponse(personList);
+    return personListToPersonResponseDTOList(personList);
+  }
+
+  @Override
+  public PersonResponseDTOList personListToPersonResponseDTOList(List<Person> personList) {
+    return new PersonResponseDTOList(
+        personList.stream().map(this::personToPersonResponseDTO).collect(Collectors.toList()));
+  }
+
+  @Override
+  public void validateGetPeopleByNameResponse(List<Person> personList) {
+    if (personList.isEmpty()) {
+      throwNoPersonByTheName();
+    }
+  }
+
+  @Override
+  public void throwNoPersonByTheName() {
+    throw new ApiBadRequestException("There is no person by the provided name");
+  }
+
+  @Override
+  public void throwNameFieldIsMissing() {
+    throw new ApiBadRequestException("name field is missing");
+  }
+
+  @Override
+  public void throwMissingField() {
+    throw new ApiBadRequestException("Missing field");
+  }
+
+  @Override
+  public void throwProvideValidPersonId() {
+    throw new ApiBadRequestException("Please provide valid personId");
+  }
+
+  @Override
+  public PersonResponseDTOList getAllPersons() {
+    List<Person> personList = getAllPersonsQuery();
+    validateGetAllPersonsResponse(personList);
+    return personListToPersonResponseDTOList(personList);
+  }
+
+  @Override
+  public void validateGetAllPersonsResponse(List<Person> personList) {
+    if (personList.isEmpty()) {
+      throw new ApiBadRequestException("There are no people entities");
+    }
+  }
+
+  @Override
+  @Transactional
+  public List<Person> getAllPersonsQuery() {
+    return entityManager.createQuery("SELECT p FROM Person p", Person.class).getResultList();
+  }
+
+  @Override
+  public void updatePerson(UpdatePersonRequestDTO requestDTO) {
+    validateUpdatePersonRequest(requestDTO);
+    Optional<Person> optionalPerson = findPersonById(requestDTO.getPersonId());
+    if (optionalPerson.isPresent()) {
+      Person person = optionalPerson.get();
+      person.setName(requestDTO.getName());
+      savePersonQuery(person);
+    } else {
+      throwProvideValidPersonId();
+    }
+  }
+
+  @Override
+  public void validateUpdatePersonRequest(UpdatePersonRequestDTO requestDTO) {
+    if (requestDTO.getName() == null || requestDTO.getPersonId() == 0) {
+      throwMissingField();
+    }
+    validateNameFormat(requestDTO.getName());
+  }
+}
